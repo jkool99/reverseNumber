e32f599791db183a34c5a8d3b2afe4bc64326c0e
jkool99 <jarredf99@aol.com>
2019-08-28 18:06:53 -0700

    Updated reverseString
    Cleaner code

--- reverseNumber.js
+++ reverseNumber.js
@@ -1,8 +1,4 @@
 reverseNumber = (input) =>{
-    //convert to string, then split into array instead of string
-    output = input.toString().split('')
-    //reverse order of arry
-    output.reverse().join('')
-    //convert array back to string, and parseInt back to number
-    return parseInt(output.join(''), 10)
+    //simplfy by returning the imput
+    return input.toString().split("").reverse().join("")
 }
